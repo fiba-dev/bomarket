3f33444e68b59fbe6be90341f72f75a049b5a457
fiba-dev <fibarrola06@gmail.com>
2022-07-01 10:53:16 -0400

    change algolia index config

--- lib/algolia.ts
+++ lib/algolia.ts
@@ -3,7 +3,7 @@ const algoliasearch = require("algoliasearch");
 const client = algoliasearch(process.env.ALGOLIA_ID, process.env.ALGOLIA_KEY);
 
 export const productIndex = client.initIndex("products");
-export const catalogue = productIndex;
+export const catalogue = client.initIndex("products");
 catalogue.setSettings({
-	searchableAttributes: ["UserId"],
+	searchableAttributes: ["UserId", "Name"],
 });
