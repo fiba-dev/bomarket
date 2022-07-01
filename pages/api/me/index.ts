a21de1dab4ceb8f64fae335694406c68c55bd88e
Mati Toledo <toledo.matias@icloud.com>
2022-06-30 21:05:17 -0300

    profile edit

--- pages/api/me/index.ts
+++ pages/api/me/index.ts
@@ -3,24 +3,41 @@ import { authMiddleware } from "lib/middlewares";
 import method from "micro-method-router";
 import { editUser, getUserFromId } from "controller/users";
 import { authMiddlewareCors } from "lib/init-middleware";
+import * as yup from "yup";
 
 async function getUser(req: NextApiRequest, res: NextApiResponse, token) {
   const user = await getUserFromId(token.userId);
 
   res.send(user.data);
 }
+
+let bodySchema = yup
+  .object()
+  .shape({
+    name: yup.string(),
+    adress: yup.string(),
+    description: yup.string(),
+    photo: yup.string(),
+    phone: yup.number(),
+  })
+  .noUnknown(true)
+  .strict();
+
 async function setUser(req: NextApiRequest, res: NextApiResponse, token) {
   const user = await getUserFromId(token.userId);
-
-  if (req.body.nombre && req.body.telefono && req.body.direccion) {
-    await editUser(req.body, user);
-  } else {
-    return res.status(400).send({
-      message: "FALTAN DATOS CAMBIO DE PRUEBA",
-    });
+  try {
+    await bodySchema.validate(req.body);
+    if (req.body) {
+      await editUser(req.body, user);
+    } else {
+      return res.status(400).send({
+        message: "FALTAN DATOS",
+      });
+    }
+    res.send(true);
+  } catch (e) {
+    res.status(422).send({ field: "body", message: e });
   }
-
-  res.send(true);
 }
 
 const handlerAuth = method({
