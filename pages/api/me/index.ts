e276babc27827e87b777464659a0e6d39239d4f3
Mati Toledo <toledo.matias@icloud.com>
2022-06-30 22:13:39 -0300

    next.config cors

--- pages/api/me/index.ts
+++ pages/api/me/index.ts
@@ -11,18 +11,6 @@ async function getUser(req: NextApiRequest, res: NextApiResponse, token) {
   res.send(user.data);
 }
 
-// let bodySchema = yup
-//   .object()
-//   .shape({
-//     name: yup.string(),
-//     adress: yup.string(),
-//     description: yup.string(),
-//     photo: yup.string(),
-//     phone: yup.number(),
-//   })
-//   .noUnknown(true)
-//   .strict();
-
 async function setUser(req: NextApiRequest, res: NextApiResponse, token) {
   const user = await getUserFromId(token.userId);
 
