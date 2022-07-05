e598c4b902dc7544ff5f8564fd401e5eab680117
fiba-dev <fibarrola06@gmail.com>
2022-07-05 10:34:52 -0400

    divide set and update products functions

--- pages/api/products/[productId].ts
+++ pages/api/products/[productId].ts
@@ -3,67 +3,76 @@ import { authMiddlewareCors } from "lib/init-middleware";
 import type { NextApiRequest, NextApiResponse } from "next";
 import methods from "micro-method-router";
 import { authMiddleware } from "lib/middlewares";
-import {
-  deleteProduct,
-  getProduct,
-  setOrUpdateProduct,
-} from "controller/products";
+import { deleteProduct, getProduct, updateProduct } from "controller/products";
 import * as yup from "yup";
 
 let querySchema = yup
-  .object()
-  .shape({
-    productId: yup.string().required(),
-  })
-  .noUnknown(true)
-  .strict();
+	.object()
+	.shape({
+		productId: yup.string().required(),
+	})
+	.noUnknown(true)
+	.strict();
+let product = yup
+	.object()
+	.shape({
+		name: yup.string(),
+		description: yup.string(),
+		price: yup.number(),
+		images: yup.string(),
+		category: yup.string(),
+		stock: yup.number(),
+	})
+	.noUnknown(true)
+	.strict();
 
 async function getHandler(req: NextApiRequest, res: NextApiResponse, userBody) {
-  try {
-    await querySchema.validate(req.query);
-    const productId = req.query.productId;
-    const resultado = await getProduct(productId);
-    if (resultado == false)
-      return res.status(404).send({ message: "Product not found" });
-    res.send(resultado);
-  } catch (error) {
-    res.status(404).send(error);
-  }
+	try {
+		await querySchema.validate(req.query);
+		const productId = req.query.productId;
+		const resultado = await getProduct(productId);
+		if (resultado == false)
+			return res.status(404).send({ message: "Product not found" });
+		res.send(resultado);
+	} catch (error) {
+		res.status(404).send(error);
+	}
 }
 
 async function patchHandler(
-  req: NextApiRequest,
-  res: NextApiResponse,
-  userBody
+	req: NextApiRequest,
+	res: NextApiResponse,
+	userBody
 ) {
-  try {
-    await querySchema.validate(req.query);
-    await setOrUpdateProduct(req.body, userBody.userId);
-  } catch (error) {
-    res.status(404).send(error);
-  }
+	try {
+		await querySchema.validate(req.query);
+		await product.validate(req.body);
+		await updateProduct(req.body, req.query.productId, userBody.userId);
+	} catch (error) {
+		res.status(404).send(error);
+	}
 
-  res.send(true);
+	res.send(true);
 }
 
 async function deleteHandler(
-  req: NextApiRequest,
-  res: NextApiResponse,
-  userBody
+	req: NextApiRequest,
+	res: NextApiResponse,
+	userBody
 ) {
-  try {
-    await querySchema.validate(req.query);
-    await deleteProduct(req.query.productId);
-    res.send(true);
-  } catch (error) {
-    res.status(404).send(error);
-  }
+	try {
+		await querySchema.validate(req.query);
+		await deleteProduct(req.query.productId);
+		res.send(true);
+	} catch (error) {
+		res.status(404).send(error);
+	}
 }
 
 const handler = methods({
-  get: getHandler,
-  patch: authMiddleware(patchHandler),
-  delete: authMiddleware(deleteHandler),
+	get: getHandler,
+	patch: authMiddleware(patchHandler),
+	delete: authMiddleware(deleteHandler),
 });
 
 export default authMiddlewareCors(handler);
